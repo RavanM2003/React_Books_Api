b2fe935847a1bc6ae6729bf615f22b73dde53f91
Ravan Mammadov <131792522+RavanM2003@users.noreply.github.com>
2024-07-30 23:41:46 +0400

    Initial Commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import SearchForm from "../SearchForm/SearchForm";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="holder">
+      <header className="header">
+        <Navbar />
+        <div className="header-content flex flex-c text-center text-white">
+          <h2 className="header-title text-capitalize">Find Your Book</h2>
+          <br />
+          <p className="header-text fs-18 fw-3">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <SearchForm/>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
